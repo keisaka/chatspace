675b6bcf54ee1ab1c2d63844d926ddcbf879ad84
keisaka <kesuike.motosaka@gmail.com>
2019-04-20 15:31:37 +0900

    capistranoの導入

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chatspace"
-set :repo_url, "git@example.com:keisaka/chatspace"
+set :repo_url, 'git@example.com:keisaka/chatspace.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
